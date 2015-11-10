bd9b97ec03c372e6d0e0f7b64bc77cb33915e920
Christian Menz <christianmenz@gmail.com>
2015-11-10 21:47:39 +0100

    read files and render simple table

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,29 +2,43 @@
 angular.module('jsLocalize', []);
 
 
-angular.module('jsLocalize', ['ngFileUpload']).controller('AppCtrl', [function() {
-  
-  this.files = [];
-  this.keys = [];
+angular.module('jsLocalize', ['ngFileUpload']).controller('AppCtrl', [function (NgTableParams) {
+  var that = this;
+
+  this.dataSet = {};
   this.downloadFiles = downloadFiles;
   this.addFiles = addFiles;
-  
+   
   //////
   
   function downloadFiles() {
-    
+
   }
-  
+
   function addFiles($files) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      console.log('Read the file', e.target.result);
-    }    
-    this.files.push($files);
-    
-    _.each($files, function(file) {
+    _.each($files, function (file) {
+
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        var fileContent, object, keys;
+
+        fileContent = e.target.result;
+        try {
+          object = JSON.parse(fileContent);
+          keys = _.keys(object);
+          _.each(keys, function (key) {
+            if (!that.dataSet[key]) {
+              that.dataSet[key] = {};
+            }
+            that.dataSet[key][file.name] = object[key];
+          })
+        } catch (error) {
+          console.log('No hablo JSON');
+        }
+      };
       reader.readAsText(file);
-    });    
+
+    });
   }
-    
+
 }]);
