8321421cc3044978473b65cce20c55b841ff79ed
Christian Menz <christianmenz@gmail.com>
2015-11-11 20:34:10 +0100

    basic stuff works now, who knew you can create zip files with JS - wow.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,33 +5,35 @@ angular.module('jsLocalize', []);
 angular.module('jsLocalize', ['ngFileUpload']).controller('AppCtrl', [function (NgTableParams) {
   var that = this;
 
-  this.dataSet = {};
+  this.files = {}; // keep track of the names and the original object
+  this.keys = []; // just all they keys
   this.downloadFiles = downloadFiles;
   this.addFiles = addFiles;
    
   //////
   
   function downloadFiles() {
-
+    var zip = new JSZip();
+    _.forOwn(this.files, function(object, fileName) {      
+      zip.file(fileName, JSON.stringify(object, null, '\t'));
+    })
+        
+    var content = zip.generate({ type: "blob" });    
+    saveAs(content, "jsLocalize.zip");
   }
 
   function addFiles($files) {
     _.each($files, function (file) {
-
       var reader = new FileReader();
+
       reader.onload = function (e) {
-        var fileContent, object, keys;
+        var fileContent, object;
 
         fileContent = e.target.result;
         try {
           object = JSON.parse(fileContent);
-          keys = _.keys(object);
-          _.each(keys, function (key) {
-            if (!that.dataSet[key]) {
-              that.dataSet[key] = {};
-            }
-            that.dataSet[key][file.name] = object[key];
-          })
+          that.keys = _.union(that.keys,_.keys(object)); 
+          that.files[file.name] = object;
         } catch (error) {
           console.log('No hablo JSON');
         }
